fdd2f73660a3b231a536e1cc8b2b8094531deee6
VladimirSTAR0911 <VladimirSTAR0911@users.noreply.github.com>
2019-03-17 23:31:16 +0500

    /

---
+++ QuickSortByKrovushka/QuickSortByKrovushka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSortByKrovushka
+{
+    public class ProgramQuickSort
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void QuickSort(double[] array, long start, long end)
+        {
+            double middle = array[(end - start) / 2 + start];// ищем средний элемент 
+            double temp;
+            long i = start, j = end;
+            while (i <= j)
+            {
+                while (array[i] < middle && i <= end) ++i;
+                while (array[j] > middle && j >= start) --j;
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    ++i; --j;
+                }
+            }
+            if (j > start) QuickSort(array, start, j);
+            if (i < end) QuickSort(array, i, end);
+        }
+    }
+}
---
+++ QuickSortByKrovushka/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuickSortByKrovushka
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var array = new double[3] { 3, 1, 2 };
+            ProgramQuickSort.QuickSort(array, 0, array.Length - 1);
+
+            Assert.IsTrue(array[0] < array[1]);
+            Assert.IsTrue(array[1] < array[2]);
+        }
+
+        [TestMethod]
+        public void TestMethod100()
+        {
+            var array = new double[100];
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 9;
+            }
+            Assert.IsTrue(array[5] == array[10]);
+        }
+        [TestMethod]
+        public void TestMethod1000()
+        {
+            var array = new double[1000];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0, 1000);
+            }
+
+            ProgramQuickSort.QuickSort(array, 0, array.Length - 1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Assert.IsTrue(array[rand.Next(0,300)] < array[rand.Next(300, 1000)]);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethodEmpity()
+        {
+            var array = new double[10];
+
+            ProgramQuickSort.QuickSort(array, 0, array.Length - 1);
+
+            Assert.IsTrue(array[0] == array[1]);
+        }
+    }
+}
