6799fa4ac8c4e012b4c347762a6b06ed079e1f45
unknown <student@server03>
2019-05-18 13:25:26 +0500

    Add Excel forms

--- SpaceFleet.Web/Controllers/UploadController.cs
+++ SpaceFleet.Web/Controllers/UploadController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -8,6 +9,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpaceFleet.Web.Models.DataAccessPostgreSqlProvider;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace SpaceFleet.Web.Controllers
 {
@@ -25,11 +28,9 @@ namespace SpaceFleet.Web.Controllers
             using (var stream = file.OpenReadStream())
             {
                 var xs = new XmlSerializer(typeof(SpaceShip));
-                var ship = (SpaceShip)xs.Deserialize(stream);
+                var ship = (SpaceShip) xs.Deserialize(stream);
 
-                
-                
-                
+
                 using (var db = new SpaceFleetDbContext())
                 {
                     var dbs = new DbSpaceShip()
@@ -72,10 +73,82 @@ namespace SpaceFleet.Web.Controllers
             List<DbSpaceShip> list;
             using (var db = new SpaceFleetDbContext())
             {
-                list = db.SpaceShips.ToList();
+                list = db.SpaceShips.Include(s=>s.Journal).ToList();
             }
 
             return View(list);
+        }
+
+
+        public ActionResult Print(int id)
+        {
+            using (var db = new SpaceFleetDbContext())
+            {
+                var ship = db.SpaceShips.Include(s1=>s1.Journal).First(s1=>s1.Id == id);
+                IWorkbook workbook =
+                    new XSSFWorkbook(System.IO.File.OpenRead("template.xlsx"));
+
+                var sheet = workbook.GetSheetAt(0);
+
+                sheet.GetRow(1).Cells[1].SetCellValue(ship.Name);
+                
+                for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+                {
+                    IRow row = sheet.GetRow(i);
+                    if (row == null) continue;
+                    if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                    var lastCellNum = row.LastCellNum;
+                    for (int j = row.FirstCellNum; j < lastCellNum; j++)
+                    {
+                        var cell = row.GetCell(j);
+                        if (cell != null)
+                        {
+                            if (cell.StringCellValue == "$PropRow")
+                            {
+                                cell.SetCellValue("Дата постройки");
+                                cell = row.GetCell(j+1) ?? row.CreateCell(j + 1);
+                                cell.SetCellValue(ship.Build);
+                                cell.CellStyle.DataFormat = 14;
+
+                                row = sheet.CopyRow(i ,i+1);
+                                i++;
+                                cell = row.GetCell(j)??row.CreateCell(j);
+                                cell.SetCellValue("Тип корабля");
+                                row.CreateCell(j + 1).SetCellValue(ship.ShipType.ToString());row = sheet.CreateRow(i++);
+                                
+                                break;
+                            }
+
+                            if (cell.StringCellValue == "$Flight")
+                            {
+                                foreach (var flight in ship.Journal)
+                                {
+                                    row = sheet.GetRow(i);
+                                    cell = row.GetCell(j);
+                                    cell.SetCellValue(flight.Crew);
+                                    cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+                                    cell.SetCellValue(flight.From);
+                                    cell = row.GetCell(j + 2) ?? row.CreateCell(j + 2);
+                                    cell.SetCellValue(flight.To);
+                                    cell = row.GetCell(j + 3) ?? row.CreateCell(j + 3);
+                                    cell.SetCellValue(flight.Passengers);
+                                    if (flight != ship.Journal.Last())
+                                    row = sheet.CopyRow(i, i + 1);
+                                    i++;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                var ms = new MemoryStream();
+                workbook.Write(ms);
+
+                ms.Position = 0;
+
+                return base.File(ms, "application/octet-stream", "ship" + id + ".xlsx");
+            }
         }
     }
 }
